d8d7343e3ad6d8d5a87578001f8aa00b70c5eff2
raul faria <raul@faria.ch>
2024-10-04 17:16:59 +0200

    Category filter

--- app/Livewire/Expenses/ExpensesList.php
+++ app/Livewire/Expenses/ExpensesList.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Expenses;
 
+use App\Enums\Category;
 use App\Enums\QuickFilterOption;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Url;
@@ -16,9 +17,34 @@ class ExpensesList extends Component
     #[Url]
     public QuickFilterOption $quick_filter = QuickFilterOption::NONE;
 
+    #[Url]
+    public ?Category $category = null;
+
     public function applyFilter()
     {
         $this->resetPage();
+    }
+
+    public function resetFilter()
+    {
+        $this->quick_filter = QuickFilterOption::NONE;
+    }
+
+    private function filterExpenses($expenses)
+    {
+        // Filter the timespan
+        if ($this->quick_filter === QuickFilterOption::MONTH) {
+            $expenses = $expenses->thisMonth();
+        } elseif ($this->quick_filter === QuickFilterOption::TODAY) {
+            $expenses = $expenses->today();
+        }
+
+        // Filter the category
+        if (! is_null($this->category)) {
+            $expenses = $expenses->where('category', $this->category);
+        }
+
+        return $expenses;
     }
 
     public function render()
@@ -27,11 +53,7 @@ class ExpensesList extends Component
             ->expenses()
             ->orderBy('date', 'desc');
 
-        if ($this->quick_filter === QuickFilterOption::MONTH) {
-            $expenses = $expenses->thisMonth();
-        } elseif ($this->quick_filter === QuickFilterOption::TODAY) {
-            $expenses = $expenses->today();
-        }
+        $expenses = $this->filterExpenses($expenses);
 
         return view('livewire.expenses.expenses-list', [
             'expenses' => $expenses->paginate(10),
--- resources/views/livewire/expenses/filter-drawer.blade.php
+++ resources/views/livewire/expenses/filter-drawer.blade.php
@@ -11,17 +11,33 @@
 
             <form wire:submit="$parent.applyFilter">
                 <div class="flex flex-col gap-2">
+
+                    {{-- Quick filter (timespan) --}}
                     <label class="form-control w-full">
                         <div class="label">
                             <span class="label-text">Quick filter</span>
                         </div>
                         <select wire:model="$parent.quick_filter" class="select select-secondary w-full max-w-xs">
-                          <option value="none">None</option>
-                          <option value="month">This month</option>
-                          <option value="today">Today</option>
+                            <option value="none">None</option>
+                            <option value="month">This month</option>
+                            <option value="today">Today</option>
                         </select>
                     </label>
-                    <button type="submit" class="btn btn-block btn-primary">Apply filter</button>
+
+                    {{-- Category filter --}}
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Category</span>
+                        </div>
+                        <select wire:model="$parent.category" class="select select-secondary w-full max-w-xs">
+                            <option value="" selected>Not filtered</option>
+                            @foreach(\App\Enums\Category::cases() as $case)
+                                <option wire:key="{{$case->value}}" value="{{$case->value}}">{{ucwords(strtolower(str_replace('_', ' ', $case->name)))}}</option>
+                            @endforeach
+                        </select>
+                    </label>
+
+                    <button type="submit" class="btn btn-block btn-primary mt-3">Apply filter</button>
                 </div>
             </form>
         </ul>
