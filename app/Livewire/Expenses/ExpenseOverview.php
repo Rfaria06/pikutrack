924ef1ddf60659f80c38a6598ed416e857c856da
raul faria <raul@faria.ch>
2024-10-04 17:23:14 +0200

    improve date display on cards

--- app/Livewire/Expenses/ExpenseOverview.php
+++ app/Livewire/Expenses/ExpenseOverview.php
@@ -15,7 +15,15 @@ class ExpenseOverview extends Component
     {
         $this->expense = $expense;
         $this->expense->amount /= 100;
-        $this->time_diff = $this->expense->date->diffForHumans();
+
+        // Check if date difference is more than 24 hours
+        if ($this->expense->date->diffInHours(now()) > 24) {
+            // Time is more than 24 hours ago -> Display the date
+            $this->time_diff = $this->expense->date->format('d. M Y H:i');
+        } else {
+            // Otherwise, show the diff
+            $this->time_diff = $this->expense->date->diffForHumans();
+        }
     }
 
     public function render()
