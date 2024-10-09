6845c233d7a937aed917fd513df9154d710e5c37
Raul Filipe Faria <raul@faria.ch>
2024-10-09 18:52:39 +0200

    Show expense

--- app/Livewire/Expenses/Show.php
+++ app/Livewire/Expenses/Show.php
@@ -3,15 +3,20 @@
 namespace App\Livewire\Expenses;
 
 use App\Models\Expense;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
 
 class Show extends Component
 {
+    use AuthorizesRequests;
+
     public Expense $expense;
 
     public function mount(Expense $expense)
     {
         $this->expense = $expense;
+
+        $this->authorize('view', $this->expense);
     }
 
     public function render()
--- resources/views/livewire/expenses/show.blade.php
+++ resources/views/livewire/expenses/show.blade.php
@@ -1,3 +1,16 @@
-<div>
-    {{$this->expense->name}}
+<div class="card card-compact w-full border-t border-primary bg-base-300 z-0 rounded-t-md shadow-primary mb-3">
+    <div class="card-body">
+        <h1 class="card-title">
+            {{$this->expense->name}}
+            <span class="badge badge-primary badge-xl">{{ucwords(strtolower(str_replace('_', ' ', $this->expense->category->name)))}}</span>
+        </h1>
+        <div class="flex flex-col gap-2">
+            <h2 class="text-lg font-bold mx-auto">{{$this->expense->amount / 100}} CHF</h2>
+            <h3 class="text-md mx-auto">{{$this->expense->date->format('d. M Y H:i')}}</h3>
+            <div class="divider"></div>
+            <button type="button" class="btn btn-block btn-secondary">Edit</button>
+            <div class="divider"></div>
+            <article>{!! nl2br($this->expense->description) !!}</article>
+        </div>
+    </div>
 </div>
