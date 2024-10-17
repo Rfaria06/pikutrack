2a1ceb84e73274117710a4cc9f5c056f28bb59f0
Raul Filipe Faria <raul@faria.ch>
2024-10-17 21:05:14 +0200

    set toast to bottom to avoid clipping in navbar

--- resources/views/components/layouts/flash.blade.php
+++ resources/views/components/layouts/flash.blade.php
@@ -1,6 +1,6 @@
-<div class="toast toast-top toast-center w-full z-50">
+<div class="toast toast-center w-full z-50">
     @if(session('message'))
-        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 5000)" x-show="show" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in duration-500" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4" role="alert" class="mx-auto alert mt-16 shadow-xl max-w-2xl">
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 5000)" x-show="show" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in duration-500" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4" role="alert" class="mx-auto alert shadow-xl max-w-2xl">
             <i class="fa-solid fa-circle-info"></i>
             <span>{{session('message')}}</span>
         </div>
