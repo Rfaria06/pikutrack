8ad98e22b242d00384731ee9286d0366efaadcd0
Raul Filipe Faria <raul@faria.ch>
2024-10-19 00:02:20 +0200

    Remove tiptap editor

--- resources/views/components/editor.blade.php
+++
@@ -1,7 +0,0 @@
-<div x-data="setupEditor(
-    $wire.entangle('{{$attributes->wire('model')->value()}}')
-  )" x-init="() => init($refs.editor)" wire:ignore {{$attributes->whereDoesntStartWith('wire:model')}}>
-
-    <button type="button" class="btn" onclick="editor.chain().focus().toggleBold().run()">Bold</button>
-    <div x-ref="editor"></div>
-</div>
