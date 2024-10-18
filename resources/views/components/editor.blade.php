9bae10aa31f314e610dc87ae8de89d7bfb2d869e
Raul Filipe Faria <raul@faria.ch>
2024-10-19 00:59:45 +0200

    Add livewire to editor

--- resources/views/components/editor.blade.php
+++ resources/views/components/editor.blade.php
@@ -1,4 +1,4 @@
-<div x-data="setupEditor('<p>Hello World! :-)</p>')" x-init="() => init($refs.element)">
+<div x-data="setupEditor($wire.entangle('{{$attributes->wire('model')->value()}}'))" x-init="() => init($refs.element)">
     <template x-if="editor">
         <div class="menu">
             <button type="button" @click="editor().chain().focus().toggleBold().run()" :class="{ 'is-active': proxy.isActive('bold') }">
